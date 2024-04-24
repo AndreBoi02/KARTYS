71e8b3c19b410a295dbb3b636d7d6e16aa55a20b
kat <lentacomputadora@gmail.com>
2024-04-24 09:54:46 -0600

    Dialogue Container

--- Assets/Kat/Scripts/MenuController.cs
+++ Assets/Kat/Scripts/MenuController.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [Header("Levels To Load")]
+    public string _newGameLevel;
+
+    public void NewGameDialogYes() {
+        SceneManager.LoadScene(_newGameLevel);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void ExitGame() {
+        Application.Quit();
     }
 }
