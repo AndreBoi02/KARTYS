b48837f4ecf4adc1c01fe4f4c90032a1b757712e
BlakesDV <dmunoz@cocoschool.mx>
2024-04-26 15:59:54 -0600

    Counter cs
    BASE script, need test

--- Assets/Blakes/Scripts/ItemBuff.cs
+++ Assets/Blakes/Scripts/ItemBuff.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class ItemBuff : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class ItemBuff : MonoBehaviour
     Growth growthScript;
     SnakeMovement snakeMov;
     SnakeMovementP2 snakeMov2;
+    ObjectPooling objectPooling;
 
     // Start is called before the first frame update
     void Start()
@@ -20,24 +22,39 @@ public class ItemBuff : MonoBehaviour
     {
         
     }
-    void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("PLayer"))
+        if (other.CompareTag("Player"))
+        {
+            SnakeMovement player = other.GetComponent<SnakeMovement>();
+            ObjectPooling objectPool = FindObjectOfType<ObjectPooling>();
+            GameObject pooledObject = objectPool.RequestObject(); // obj pooling
+            if (pooledObject != null)
             {
-                //other.GetComponent<SnakeMovement>().m_snakeBodySize += numeroDelValorDeLaComida; //serialized field en vars PRIVATE
+                pooledObject.SetActive(true);
+                pooledObject.transform.position = transform.position;
             }
-        if (other.CompareTag("PLayer2"))
-            {
-                other.GetComponent<SnakeMovementP2>().m_snakeBodySize += numeroDelValorDeLaComida;
-            }
-        //if (other.CompareTag("Buff"))
-        //{
-        //    growthScript.GrowBody(1);
-        //}
-        //else if (other.CompareTag("Obstacle"))
-        //{
-        //    //growthScript.RemoveBodyPart();
-        //}
+            gameObject.SetActive(false);
+        }
     }
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.CompareTag("PLayer"))
+    //        {
+    //            //other.GetComponent<SnakeMovement>().m_snakeBodySize += numeroDelValorDeLaComida; //serialized field en vars PRIVATE
+    //        }
+    //    if (other.CompareTag("PLayer2"))
+    //        {
+    //            other.GetComponent<SnakeMovementP2>().m_snakeBodySize += numeroDelValorDeLaComida;
+    //        }
+    //    //if (other.CompareTag("Buff"))
+    //    //{
+    //    //    growthScript.GrowBody(1);
+    //    //}
+    //    //else if (other.CompareTag("Obstacle"))
+    //    //{
+    //    //    //growthScript.RemoveBodyPart();
+    //    //}
+    //}
 
 }
