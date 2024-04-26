179dc8e151318c4f0b8623f6c25a47e888fd7746
DCRockStarrTV <Diegox.mc7@gmail.com>
2024-04-26 14:58:14 -0600

    UI

--- Assets/Kat/Scripts/MenuController.cs
+++ Assets/Kat/Scripts/MenuController.cs
@@ -68,7 +68,7 @@ public class MenuController : MonoBehaviour
 
 
     public void NewGameDialogYes() {
-        SceneManager.LoadScene(_newGameLevel);
+        SceneManager.LoadScene("DC");
     }
 
     public void ExitGame() {
