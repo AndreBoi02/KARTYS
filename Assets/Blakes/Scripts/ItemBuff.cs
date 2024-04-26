cc1942cd52f258044aeaffb90876467175a4baae
BlakesDV <dmunoz@cocoschool.mx>
2024-04-26 13:39:38 -0600

    Script changes
    Scene and script chamges to try the food buff

--- Assets/Blakes/Scripts/ItemBuff.cs
+++ Assets/Blakes/Scripts/ItemBuff.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class ItemBuff : MonoBehaviour
 {
+    int numeroDelValorDeLaComida = 1;
     Growth growthScript;
+    SnakeMovement snakeMov;
+    SnakeMovementP2 snakeMov2;
 
     // Start is called before the first frame update
     void Start()
@@ -19,13 +22,22 @@ public class ItemBuff : MonoBehaviour
     }
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Buff"))
-        {
-            growthScript.GrowBody(1);
-        }
-        else if (other.CompareTag("Obstacle"))
-        {
-            //growthScript.RemoveBodyPart();
-        }
+        if (other.CompareTag("PLayer"))
+            {
+                //other.GetComponent<SnakeMovement>().m_snakeBodySize += numeroDelValorDeLaComida; //serialized field en vars PRIVATE
+            }
+        if (other.CompareTag("PLayer2"))
+            {
+                other.GetComponent<SnakeMovementP2>().m_snakeBodySize += numeroDelValorDeLaComida;
+            }
+        //if (other.CompareTag("Buff"))
+        //{
+        //    growthScript.GrowBody(1);
+        //}
+        //else if (other.CompareTag("Obstacle"))
+        //{
+        //    //growthScript.RemoveBodyPart();
+        //}
     }
+
 }
